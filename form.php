1663ffecc7400591a46a9de4838f9dcc7a8c9e6c
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-09-24 22:49:42 -0700

    LEVEL 1
    LEVEL 1 - Validating the Form Values

--- form.php
+++ form.php
@@ -1,12 +1,30 @@
 <?php
 	if(isset($_POST['submit']))
 	{ 
+		$name = array("awekarin","yonglek");
+
+		$min = 5;
+		$max = 10;
+
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 		
-		echo $username;
-		echo "<br/>";
-		echo $password;
+		if(strlen($username)<$min)
+		{
+			echo "username harus memiliki panjang 5 atau lebih";
+		}
+		if(strlen($username)>$max)
+		{
+			echo "username tidak boleh lebih panjang dari 10";
+		}
+		if(!in_array($username, $name))
+		{
+			echo "maaf, akses ditolak";
+		}
+		else
+		{
+			echo "selamat datang";
+		}
 	}
 ?>
 
