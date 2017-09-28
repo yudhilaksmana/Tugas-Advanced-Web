783c7d7c162e108cbf132eda41310459c87c0ae5
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-09-28 00:30:23 -0700

    LEVEL 2
    LEVEL 2 - Creating a Database, Tables and Inserting Data in PHPMyAdmin

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-md-6">
+				<form action="login.php" method="post">
+					<div class="form-group">
+						<label for="username">Username</label>
+						<input type="text" name="username" class="form-control">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" name="password" class="form-control">
+					</div>
+					<input type="submit" name="submit" value="Submit" class="btn btn-primary" />
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
