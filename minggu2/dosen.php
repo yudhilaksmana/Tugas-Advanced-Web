c8ff28e685aee1378fb8fa1ec7a46ed493d30217
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-10-05 15:00:21 -0700

    Dosen C(R)UD
    Fungsi read dosen

--- minggu2/dosen.php
+++ minggu2/dosen.php
@@ -1,20 +1,36 @@
-<?php //filename: mahasiswa.php
+<?php
+	include("db.php");
+
+	$query = "SELECT * FROM dosen";
+	$hasil = mysqli_query($koneksi, $query);
 ?>
 
-<h1> Data Dosen </h1>
+<h1>Data Dosen</h1>
+<a href="template.php?page=formdosen&action=add">Tambah Data</a>
 <table>
 	<thead>
 		<tr>
-			<td> No.</td>
-			<td>Kode Dosen</td>
-		<td>Nama</td>
+			<th>No.</th>
+			<th>Kode Dosen</th>
+			<th>Nama</th>
+			<th>Action</th>
 		</tr>
 	</thead>
 	<tbody>
+		<?php
+			while($row = mysqli_fetch_assoc($hasil)){
+		?>
 		<tr>
-			<td> 1 </td>
-			<td>L007</td>
-			<td> John Doe </td>
+			<td><?php echo $row['id']; ?></td>
+			<td><?php echo $row['kode_dosen']; ?></td>
+			<td><?php echo $row['nama']; ?></td>
+			<td>
+				<a href="template.php?page=formdosen&id= <?php echo $row['id'];?> &action=edit">Edit</a>
+				<a href="proses_dosen.php?action=delete&id= <?php echo $row['id'];?> ">Delete</a>
+			</td>
 		</tr>
+		<?php
+			}
+		?>
 	</tbody>
 </table>
