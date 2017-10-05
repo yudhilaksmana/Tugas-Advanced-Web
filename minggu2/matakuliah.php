42fae555d11297b7d207e50bfbc90d5c61cb244a
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-10-05 15:56:10 -0700

    Matakuliah C(R)UD
    Fungsi read matakuliah

---
+++ minggu2/matakuliah.php
@@ -0,0 +1,36 @@
+<?php
+	include("db.php");
+
+	$query = "SELECT * FROM matakuliah";
+	$hasil = mysqli_query($koneksi, $query);
+?>
+
+<h1>Data Mata Kuliah</h1>
+<a href="template.php?page=formmatakuliah&action=add">Tambah Data</a>
+<table>
+	<thead>
+		<tr>
+			<th>No.</th>
+			<th>Kode Mata Kuliah</th>
+			<th>Nama</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			while($row = mysqli_fetch_assoc($hasil)){
+		?>
+		<tr>
+			<td><?php echo $row['id']; ?></td>
+			<td><?php echo $row['kode_matkul']; ?></td>
+			<td><?php echo $row['nama']; ?></td>
+			<td>
+				<a href="template.php?page=formdosen&id= <?php echo $row['id'];?> &action=edit">Edit</a>
+				<a href="proses_dosen.php?action=delete&id= <?php echo $row['id'];?> ">Delete</a>
+			</td>
+		</tr>
+		<?php
+			}
+		?>
+	</tbody>
+</table>
--- minggu2/template.php
+++ minggu2/template.php
@@ -16,7 +16,8 @@
 		<div id="header"><h3>Sistem Informasi Kampus</h3></div>
 		<div id="sidebar">
 			<a href="template.php?page=mahasiswa">Mahasiswa</a>&nbsp|
-			<a href="template.php?page=dosen">Dosen</a>
+			<a href="template.php?page=dosen">Dosen</a> |
+			<a href="template.php?page=matakuliah">Mata Kuiah</a>
 		</div>
 		<div id="content">
 			Ini adalah konten
