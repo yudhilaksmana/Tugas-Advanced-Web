13a53eb13c11038155aeb1ccd82b8a70b7e3e76c
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-10-05 15:34:10 -0700

    Dosen CR(U)D
    Fungsi update dosen

--- minggu2/formdosen.php
+++ minggu2/formdosen.php
@@ -1,13 +1,29 @@
 <?php
-	echo "<h1>Add Dosen</h1>";
+	if ($_GET['action'] == "edit")
+	{
+		echo "<h1> Edit Dosen </h1>";
+		include ("db.php");
+		$query = "SELECT * FROM dosen WHERE id=$_GET[id]";
+		$hasil = mysqli_query ($koneksi, $query);
+		$row = mysqli_fetch_assoc($hasil);
+	}
+	else
+	{
+		echo "<h1>Add Dosen</h1>";
+		$row ['kode_dosen'] = "";
+		$row ['nama'] = "";
+		$row ['jurusan'] = "";
+		$row ['id'] = "";
+	}
 ?>
 
 <form action = "proses_dosen.php?action=<?php echo $_GET['action']; ?>" method="post">
 	Kode Dosen:
-	<input type="text" name="kode_dosen" value=""/>
+	<input type="text" name="kode_dosen" value="<?php echo $row['kode_dosen']; ?>"/>
 	<br/>
 	Nama Dosen:
-	<input type="text" name="nama" value=""/>
+	<input type="text" name="nama" value="<?php echo $row['nama']; ?>"/>
 	<br/>
+	<input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
 	<input type="submit" name="submit" />
 </form>
--- minggu2/proses_dosen.php
+++ minggu2/proses_dosen.php
@@ -6,6 +6,11 @@
 		//2. Query
 		$query ="INSERT INTO dosen (kode_dosen, nama) VALUES ('$_POST[kode_dosen]', '$_POST[nama]')";
 	}
+	else if($_GET['action'] == "edit")
+	{
+		//2.Query
+		$query = "UPDATE dosen SET kode_dosen = '$_POST[kode_dosen]', nama = '$_POST[nama]' WHERE id=$_POST[id]";
+	}
 
 	mysqli_query ($koneksi, $query);
 	
