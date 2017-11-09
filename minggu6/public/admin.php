ecfef0e7c6778044e11f8599c2034bc1aa3f1c6d
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-11-09 18:34:40 +0700

    Minggu 6
    097 - Creating and styling the first page

---
+++ minggu6/public/admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>untitled</title>
+		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="header">
+			<h1>Widget Corp</h1>
+		</div>
+		<div id="main">
+			<div id="navigation">
+				&nbsp;
+			</div>
+			<div id="page">
+				<h2>Admin Menu</h2>
+				<p>Welcome to the admin area.</p>
+				<ul>
+					<li><a href="manage_content.php">Manage Website Content</a></li>
+					<li><a href="manage_admins.php">Manage Admin Users</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="footer">Copyright 20xx, Widget Corp</div>
+	</body>
+</html>
