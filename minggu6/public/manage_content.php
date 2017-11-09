b8dcde6818ab90bdd2a726f91f9c2d85a482f6b6
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-11-10 03:13:20 +0700

    Minggu 6
    102 - Selecting pages from navigation

--- minggu6/public/manage_content.php
+++ minggu6/public/manage_content.php
@@ -2,6 +2,20 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
+<?php
+	if (isset($_GET["subject"])) {
+		$selected_subject_id = $_GET["subject"];
+		$selected_page_id = null;
+	} else if (isset($_GET["page"])) {
+		$selected_page_id = $_GET["page"];
+		$selected_subject_id = null;
+	} else {
+		$selected_page_id = null;
+		$selected_subject_id = null;
+	}
+
+?>
+
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
@@ -10,17 +24,19 @@
 		while($subject = mysqli_fetch_assoc($subject_set)) {
 		?>  
 			<li>
-				<?php echo $subject["menu_name"]; ?>
+				<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
 				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
 				<ul class="pages">
 				<?php
 					while($page = mysqli_fetch_assoc($page_set)) {
 				?>  
-					<li><?php echo $page["menu_name"]; ?></li>
+					<li>
+						<a href="manage_content.php?page="<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
+					</li>
 				<?php
 					}
 				?>
-				<?php mysqli_free_result($page_set); ?>
+				<?php mysqli_free_result($page _set); ?>
 				</ul>
 			</li>
 		<?php
@@ -31,6 +47,8 @@
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
+		<?php echo $selected_subject_id; ?><br />
+		<?php echo $selected_page_id; ?>
 	</div>
 </div>
 
