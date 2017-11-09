28cdaf803feae7af00ed2d727a5b633c62021450
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-11-10 01:53:12 +0700

    Minggu 6
    100 - Adding pages to the navigation subjects

--- minggu6/public/manage_content.php
+++ minggu6/public/manage_content.php
@@ -15,39 +15,54 @@
 ?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-<?php
-	// 2. Perform database query
-	$query = "SELECT * ";
-	$query .= "FROM subjects ";
-	$query .= "WHERE visible = 1 ";
-	$query .= "ORDER BY position ASC";
-	$result = mysqli_query($connection, $query);
-	confirm_query($result);
-?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
 		<?php
-		// 3. Use returned data (if any)
-		while($subject = mysqli_fetch_assoc($result)) {
-			// output data from each row
+			$query = "SELECT * ";
+			$query .= "FROM subjects ";
+			$query .= "WHERE visible = 1 ";
+			$query .= "ORDER BY position ASC";
+			$subject_set = mysqli_query($connection, $query);
+			confirm_query($subject_set);
 		?>
-			<li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
+		<?php
+		while($subject = mysqli_fetch_assoc($subject_set)) {
+		?>
+			<li>
+				<?php echo $subject["menu_name"]; ?>
+				<?php
+					$query = "SELECT * ";
+					$query .= "FROM pages ";
+					$query .= "WHERE visible = 1 ";
+					$query .= "AND {subject["id"]} ";
+					$query .= "ORDER BY position ASC";
+					$page_set = mysqli_query($connection, $query);
+					confirm_query($page_set);
+				?>
+				<ul class="pages">
+					<?php
+						while($page = mysqli_fetch_assoc($page_set)) {
+					?>
+						<li><?php echo $page["menu_name"];?></li>
+					<?php
+						}
+					?>
+					<?php mysqli_free_result($page_set);?>
+				</ul>
+			</li>
 			<?php
 		}
 		?>
+		<?php mysqli_free_result($subject_set);?>
 		</ul>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
 	</div>
 </div>
-<?php
-	// 4. Release returned data
-	mysqli_free_result($result);
-?>
 <?php include("../includes/layouts/footer.php"); ?>
 <?php
 	// 5. Close database connection
