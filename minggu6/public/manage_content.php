928c31eadf34d656a77c4f292c484598e64ac629
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-11-10 03:17:04 +0700

    Minggu 6
    103 - Highliting the current page

--- minggu6/public/manage_content.php
+++ minggu6/public/manage_content.php
@@ -22,14 +22,28 @@
 		<?php $subject_set = find_all_subjects(); ?>
 		<?php
 		while($subject = mysqli_fetch_assoc($subject_set)) {
-		?>  
+		?>
+			<?php
+				echo "<li";
+				if($subject["id"] == $selected_subject_id){
+					echo " class=\"selected\"";
+				}
+				echo ">";
+			?>
 			<li>
 				<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
 				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
 				<ul class="pages">
 				<?php
 					while($page = mysqli_fetch_assoc($page_set)) {
-				?>  
+				?> 
+					<?php
+						echo "<li";
+						if($page["id"] == $selected_page_id){
+							echo " class=\"selected\"";
+						}
+						echo ">";
+					?> 
 					<li>
 						<a href="manage_content.php?page="<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
 					</li>
