ae99f5b9fb7e0842f14eae0d800345238f605a7d
Yudhi Laksmana Putra <yudhilaksmana@gmail.com>
2017-11-10 02:10:46 +0700

    Minggu 6
    101 - Rfactoring the navigation

--- minggu6/includes/functions.php
+++ minggu6/includes/functions.php
@@ -4,4 +4,29 @@
 			die("Database query failed.");
 		}
 	}
+
+	function find_all_subjects() {
+		global $connection;
+		
+		$query = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "ORDER BY position ASC";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		return $subject_set;
+	} 
+	
+	function find_pages_for_subject($subject_id) {
+		global $connection;
+		
+		$query = "SELECT * ";
+		$query .= "FROM pages  ";
+		$query .= "AND subject_id = {$subject_id} ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "ORDER BY position ASC";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set);
+		return $page_set; 
+	}
 ?>
--- minggu6/public/manage_content.php
+++ minggu6/public/manage_content.php
@@ -1,18 +1,3 @@
-<?php
-	// 1. Create a database connection
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$dbname = "db_minggu6";
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
-	// Test ir connection succeeded
-	if(mysqli_connect_errno()) {
-		die("Database connection failed : " .
-			mysqli_connect_error() .
-			" (" . mysqli_connect_errno() . ")");
-	}
-?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
@@ -20,51 +5,34 @@
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
-		<?php
-			$query = "SELECT * ";
-			$query .= "FROM subjects ";
-			$query .= "WHERE visible = 1 ";
-			$query .= "ORDER BY position ASC";
-			$subject_set = mysqli_query($connection, $query);
-			confirm_query($subject_set);
-		?>
+		<?php $subject_set = find_all_subjects(); ?>
 		<?php
 		while($subject = mysqli_fetch_assoc($subject_set)) {
-		?>
+		?>  
 			<li>
 				<?php echo $subject["menu_name"]; ?>
-				<?php
-					$query = "SELECT * ";
-					$query .= "FROM pages ";
-					$query .= "WHERE visible = 1 ";
-					$query .= "AND {subject["id"]} ";
-					$query .= "ORDER BY position ASC";
-					$page_set = mysqli_query($connection, $query);
-					confirm_query($page_set);
-				?>
+				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
 				<ul class="pages">
-					<?php
-						while($page = mysqli_fetch_assoc($page_set)) {
-					?>
-						<li><?php echo $page["menu_name"];?></li>
-					<?php
-						}
-					?>
-					<?php mysqli_free_result($page_set);?>
+				<?php
+					while($page = mysqli_fetch_assoc($page_set)) {
+				?>  
+					<li><?php echo $page["menu_name"]; ?></li>
+				<?php
+					}
+				?>
+				<?php mysqli_free_result($page_set); ?>
 				</ul>
 			</li>
-			<?php
-		}
+		<?php
+			}
 		?>
-		<?php mysqli_free_result($subject_set);?>
+		<?php mysqli_free_result($subject_set); ?>
 		</ul>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
 	</div>
 </div>
+
+
 <?php include("../includes/layouts/footer.php"); ?>
-<?php
-	// 5. Close database connection
-	mysqli_close($connection);
-?>
